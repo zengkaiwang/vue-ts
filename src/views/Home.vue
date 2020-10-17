58d64be8c16100f7ee894552fa886d69970217d9
scaler <zengkaiwang@yeah.net>
2020-10-17 22:59:00 +0800

    可视化预览组件npm包发布测试成功

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,96 +1,26 @@
 <template>
   <div class="home">
-    <!-- <vue-toast></vue-toast> -->
-    <vue-toast-ugly></vue-toast-ugly>
     <div>我是home页面</div>
-    
-    <div>测试语言：{{language}}</div>
+    <npm-home></npm-home>
+    <preview-comp></preview-comp>
 
-    <!-- <quill-editor v-model="content"
-      ref="myQuillEditor"
-      :options="editorOption"
-      @change="onEditorBlur($event)"
-      @focus="onEditorFocus($event)"
-      @ready="onEditorReady($event)">
-    </quill-editor> -->
-    
-    <el-button type="primary">主要按钮</el-button>
-    <div :style="{ color: 'orange' }">
-      <icon
-        data="@icon/icon-dashboard.svg"
-        width="40"
-        height="40"
-        color="blue"
-        dir="right"
-      />
-    </div>
+    <!-- 文本编辑器\npm包\获取系统语言\新版vue-svg包的使用 -->
+    <!-- <quill-edtor></quill-edtor> -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+import { Component, Vue } from 'vue-property-decorator'
+import { Home, PreviewComp } from '@aiot/studio-render'
+// import QuillEdtor from './quill-editor.vue'
 
-//  const { Person } = require('@scaler/test01')
-//  import Person from '@scaler/test01'
- import { Person } from '@scaler/test01'
-
- import vueToast from '@scaler/npm-vue'
- import vueToastUgly from '@scaler/npm-vue-ugly'
-
- import * as Quill from 'quill' // 引入编辑器
- import ImageResize from 'quill-image-resize-module'
- Quill.register('modules/imageResize', ImageResize)
-
-export default {
-  name: 'Home',
-  data: function() {
-    return {
-      language: '',
-      editorOption: {
-        placeholder: 'Hello World',
-        modules: {
-          toolbar: {
-            container: [
-              [{ size: ['small', false, 'large'] }],
-              [{ color: [] }, { background: [] }], 
-              [{ font: [] }],
-              [{ align: [] }],
-              ['bold', 'italic'],
-              [{ list: 'ordered' }, { list: 'bullet' }],
-              ['link', 'image'],
-              [{ direction: 'rtl' }]
-            ]
-          },
-          imageResize: {
-            displayStyles: {
-              backgroundColor: 'black',
-              border: 'none',
-              color: 'white'
-            },
-            modules: ['Resize', 'DisplaySize', 'Toolbar']
-          }
-        }
-    }
-    }
-  },
+@Component({
   components: {
-    // HelloWorld
-    vueToast,
-    vueToastUgly
-  },
-  created() {
-    console.log(Person)
-    const person = new Person()
-    person.hello()
-    
-    this.language = navigator.language
-  },
-  methods: {
-    onEditorBlur(e) {
-      console.log(e)
-    }
+    'preview-comp': PreviewComp,
+    'npm-home': Home
+    // 'quill-edtor': QuillEdtor
   }
-
+})
+export default class HomePage extends Vue {
 }
 </script>
---
+++ src/views/quill-editor.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="home">
+    <!-- <vue-toast></vue-toast> -->
+    <vue-toast-ugly></vue-toast-ugly>
+    
+    <div>测试语言：{{language}}</div>
+
+    <!-- <quill-editor v-model="content"
+      ref="myQuillEditor"
+      :options="editorOption"
+      @change="onEditorBlur($event)"
+      @focus="onEditorFocus($event)"
+      @ready="onEditorReady($event)">
+    </quill-editor> -->
+    
+    <el-button type="primary">主要按钮</el-button>
+    <div :style="{ color: 'orange' }">
+      <icon
+        data="@icon/icon-dashboard.svg"
+        width="40"
+        height="40"
+        color="blue"
+        dir="right"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+//  const { Person } = require('@scaler/test01')
+//  import Person from '@scaler/test01'
+ import { Person } from '@scaler/test01'
+
+ import vueToast from '@scaler/npm-vue'
+ import vueToastUgly from '@scaler/npm-vue-ugly'
+
+ import * as Quill from 'quill' // 引入编辑器
+ import ImageResize from 'quill-image-resize-module'
+ Quill.register('modules/imageResize', ImageResize)
+
+export default {
+  name: 'Home',
+  data: function() {
+    return {
+      language: '',
+      editorOption: {
+        placeholder: 'Hello World',
+        modules: {
+          toolbar: {
+            container: [
+              [{ size: ['small', false, 'large'] }],
+              [{ color: [] }, { background: [] }], 
+              [{ font: [] }],
+              [{ align: [] }],
+              ['bold', 'italic'],
+              [{ list: 'ordered' }, { list: 'bullet' }],
+              ['link', 'image'],
+              [{ direction: 'rtl' }]
+            ]
+          },
+          imageResize: {
+            displayStyles: {
+              backgroundColor: 'black',
+              border: 'none',
+              color: 'white'
+            },
+            modules: ['Resize', 'DisplaySize', 'Toolbar']
+          }
+        }
+    }
+    }
+  },
+  components: {
+    // HelloWorld
+    vueToast,
+    vueToastUgly
+  },
+  created() {
+    console.log(Person)
+    const person = new Person()
+    person.hello()
+    
+    this.language = navigator.language
+  },
+  methods: {
+    onEditorBlur(e) {
+      console.log(e)
+    }
+  }
+
+}
+</script>
