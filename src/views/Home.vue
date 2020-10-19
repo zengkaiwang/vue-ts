9cf843439d77a5c89b57976af867012d7d049739
scaler <zengkaiwang@yeah.net>
2020-10-19 10:14:10 +0800

    测试预览npm包

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <div>我是home页面</div>
-    <npm-home></npm-home>
+    <!-- <npm-home></npm-home> -->
     <preview-comp></preview-comp>
 
     <!-- 文本编辑器\npm包\获取系统语言\新版vue-svg包的使用 -->
